19e79e9b085d4b00343b0ab1cf2ff55ec3bf68fb
johnclc <johnclc@users.noreply.github.com>
2016-07-06 15:12:22 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -45,18 +45,13 @@ variable "aws_region" {
 }
 
 provider "aws" {
-  #access_key = "AKIAJMR2WFA4GCAXVEAA"
   access_key = "${var.aws_access_key}"
-
-  #secret_key = "FiHPBlBd7OdudJNSjaOj6BCI0CyKC6kFM5ImLYAl"
   secret_key = "${var.aws_secret_key}"
-
-  #region     = "eu-central-1"
   region = "${var.aws_region}"
 }
 
 resource "aws_instance" "web" {
-  count                  = "2"
+  count                  = "1"
   ami                    = "ami-74ee001b"
   instance_type          = "t2.micro"
   subnet_id              = "subnet-9e15f8f6"
